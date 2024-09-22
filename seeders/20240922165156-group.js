8090a04ebb008684e6f1874d70e3fce628828a5f
RotemCarmon <tetch6@gmail.com>
2024-09-22 22:31:37 +0300

    refactor: add antoher group to seed file

--- seeders/20240922165156-group.js
+++ seeders/20240922165156-group.js
@@ -7,9 +7,18 @@ module.exports = {
       'group',
       [
         {
-          id:1,
-          name: 'Admin',
-          description: 'Admin group',
+          id: 1,
+          name: 'Test Group',
+          description: 'Test Group Description',
+          status: 'active',
+          createdBy: 1,
+          createdAt: new Date(new Date().getTime() - 10 * 60000),
+          updatedAt: new Date(new Date().getTime() - 10 * 60000),
+        },
+        {
+          id: 2,
+          name: 'Test Group 2',
+          description: 'Test Group 2 Description!',
           status: 'active',
           createdBy: 1,
           createdAt: new Date(),
