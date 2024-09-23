59204a21236daba2dbfbdbb6f3fc5833f8afa929
RotemCarmon <tetch6@gmail.com>
2024-09-23 20:09:08 +0300

    add log in auth.controller

--- src/api/auth/auth.controller.ts
+++ src/api/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import errors from '@error';
 import { authService } from './auth.service';
+import logger from '@logger';
 const { UnauthorizedError } = errors;
 
 async function login(req: Request, res: Response, next: NextFunction) {
   try {
     const { email, password } = req.body;
     const token = await authService.login(email, password);
+    logger.info(`[AUTH CONTROLLER] User logged in: ${email}`);
     res.json(token);
   } catch (error: any) {
     next(new UnauthorizedError(error.message));
   }
 }
-
 
 export default {
   login,
