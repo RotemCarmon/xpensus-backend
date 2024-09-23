17961e635487e5aa4fd0d8ab679f7bae7306e481
RotemCarmon <tetch6@gmail.com>
2024-09-23 17:40:19 +0300

    refactor: export new models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,11 @@
 import { initModel as groupInitModel } from './entities/group.model';
 import { initModel as userInitModel } from './entities/user.model';
+import { initModel as groupMemberInitModel } from './entities/groupMember.model';
+import { initModel as groupInvitationInitModel } from './entities/groupInvitation.model';
 
 export default {
   group: groupInitModel,
   user: userInitModel,
+  groupInvitation: groupInvitationInitModel,
+  groupMember: groupMemberInitModel,
 };
