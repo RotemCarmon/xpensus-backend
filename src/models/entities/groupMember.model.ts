9f0f6594ad07940be72519714012d050af2bb703
RotemCarmon <tetch6@gmail.com>
2024-09-23 16:54:49 +0300

    feat: add group.model.ts file

---
+++ src/models/entities/groupMember.model.ts
@@ -0,0 +1,74 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+import { GroupMember as GroupMemberAttributes, GroupMemberStatus } from '../../types/GroupMember';
+import dbService from '@src/services/db.service';
+
+interface GroupMemberCreationAttributes extends Optional<GroupMemberAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
+
+export class GroupMember extends Model<GroupMemberAttributes, GroupMemberCreationAttributes> implements GroupMemberAttributes {
+  id!: number;
+  groupId!: number;
+  userId!: number;
+  status!: GroupMemberStatus;
+  joinedAt!: Date | null;
+  leftAt!: Date | null;
+  createdAt!: Date;
+  updatedAt!: Date;
+}
+
+export function initModel(sequelize: Sequelize) {
+  GroupMember.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      groupId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        references: {
+          model: dbService.db.group,
+          key: 'id',
+        },
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        references: {
+          model: dbService.db.user,
+          key: 'id',
+        },
+      },
+      status: {
+        type: DataTypes.ENUM(),
+        values: Object.values(GroupMemberStatus),
+        allowNull: false,
+        defaultValue: GroupMemberStatus.INVITED,
+      },
+      joinedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      leftAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'GroupMember',
+      tableName: 'group_member',
+      timestamps: true,
+    }
+  );
+}
