21af5c766c467d77b1d1c1b6087863ef2a82a42a
RotemCarmon <tetch6@gmail.com>
2024-09-23 11:53:42 +0300

    feat: add user model to model exports

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,7 @@
 import { initModel as groupInitModel } from './entities/group.model';
+import { initModel as userInitModel } from './entities/user.model';
 
 export default {
   group: groupInitModel,
+  user: userInitModel,
 };
