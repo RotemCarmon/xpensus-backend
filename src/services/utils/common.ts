922e66cafe920f4ed990ee63f5aeb3ad0f9ff47c
RotemCarmon <tetch6@gmail.com>
2024-09-23 23:23:02 +0300

    add generateUniqueToken util function

--- src/services/utils/common.ts
+++ src/services/utils/common.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import { randomBytes } from 'crypto';
 import config from '@src/config';
 
 export function hashedPassword(password: string) {
@@ -6,4 +7,9 @@ export function hashedPassword(password: string) {
   return bcrypt.hash(password, saltRounds);
 }
 
+export function generateUniqueToken(): string {
+  const randomPart = randomBytes(16).toString('hex');
+  const timestamp = Date.now().toString(36);
+  return `${randomPart}-${timestamp}`;
+}
 
