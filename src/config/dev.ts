0ac196addf8698c88211464fc5db20bf75651558
RotemCarmon <tetch6@gmail.com>
2024-09-23 20:09:39 +0300

    add accessTokenSecret in config files

--- src/config/dev.ts
+++ src/config/dev.ts
@@ -6,10 +6,11 @@ export const dev: Config = {
   db: {
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'rotemc',
-    type: process.env.DB_TYPE,
+    type: process.env.DB_TYPE || 'postgres',
     name: process.env.DB_NAME,
     port: process.env.DB_PORT || 5432,
   },
 
   encryptSalt: process.env.ENCRYPT_SALT || 10,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET
 };
